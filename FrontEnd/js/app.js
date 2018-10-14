17cfc03b6c021d1a91eccae2b9b166777ffedd01
zutt <yannlederrey@gmail.com>
2018-10-14 11:53:14 +0200

    afficher noms des edges

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,25 +1,51 @@
 
 function getUserContributors(username){
-    return fetch(`http://localhost:3100/collaborateurs/${username}`)
+    return fetch(`http://localhost:3200/collaborateurs/${username}`)
         .then(res => {
             console.log('1');
             return res.json();
         });
 }
 
-function requestData() {
 
-    var username=document.getElementById('name').value;
-    getUserContributors(username)
-    .then(value => {
-        console.log(value);
-        return alchemy.begin({
-            dataSource: value,
-            nodeCaption: 'type',
-            nodeMouseOver: 'caption',
-            cluster: true,
-            clusterColours: ["#1B9E77","#D95F02","#7570B3","#E7298A","#66A61E","#E6AB02"]}
-        )
-    })
+function clearAlchemy() {
+    return alchemy.begin({
+        dataSource: "{}",
+        nodeCaption: 'type',
+        nodeMouseOver: 'caption',
+        cluster: true,
+        clusterColours: ["#1B9E77","#D95F02","#7570B3","#E7298A","#66A61E","#E6AB02"]}
+    )
 }
+
+function requestData() {
+    var div = document.createElement("div");
+    div.setAttribute("id","alchemyTextDiv");
+    var element = document.getElementById("alchemy");
+    element.appendChild(div);
+    /*
+    var alchemySVG = element.getElementsByTagName("svg");
+    var elementSVG = alchemySVG[0];
+    elementSVG.appendChild(div);*/
+        var username = document.getElementById('name').value;
+        getUserContributors(username)
+            .then(value => {
+                var config = {
+                    dataSource: value,
+                    nodeCaption: 'type',
+                    nodeMouseOver: 'caption',
+                    cluster: true,
+                    clusterColours: ["#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02"],
+                    "edgeClick": function (edge) {
+                        console.log(edge);
+                        div.innerHTML = "List project : " + edge._properties.caption;
+                        return edge._properties.source;
+                    },
+                };
+                console.log(value);
+                return alchemy.begin(config);
+            });
+}
+
+
 
